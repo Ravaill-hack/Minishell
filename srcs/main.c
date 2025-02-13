20df417d7c960a996ebee2adad01620c48c77bf7
lmatkows <lmatkows@student.42perpignan.fr>
2025-02-13 15:57:12 +0100

    en cours

--- srcs/main.c
+++ srcs/main.c
@@ -6,23 +6,53 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 10:26:14 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/02/13 11:15:33 by lmatkows         ###   ########.fr       */
+/*   Updated: 2025/02/13 15:56:57 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	**ft_get_env(char **env_shell)
+{
+	char	**env;
+	int		i;
+
+	i = 0;
+	env = (char **)malloc(sizeof(char *));
+	if (!env)
+		return (NULL);
+	while (env_shell[i])
+	{
+		env[i] = ft_strdup(env_shell[i]);
+
+	}
+}
+
+t_minishell	*ft_init_var(char **env)
+ {
+	t_minishell	*var;
+
+	var = (t_minishell *)malloc(sizeof(t_minishell));
+	if (!var)
+		return (NULL);
+	var->env = ft_get_env(env);
+	var->line = NULL;
+	return (var);
+ }
+
 int	main(int argc, char **argv, char **env)
 {
-	t_minishell	var;
+	t_minishell	*var;
 
-	(void)argc,
-	(void)argv,
+	(void)argc;
+	(void)argv;
 	var = ft_init_var(env);
 	while (1)
 	{
-		var.line = readline(PROMPT);
+		var->line = readline(PROMPT);
+		add_history(var->line);
+		//ft_error_line(var->line);
 	}
-	printf("%s\n", line);
-	free (line);
+	printf("%s\n", var->line);
+	free (var->line);
 }
