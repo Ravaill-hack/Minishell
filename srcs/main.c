03a7e82161af10450acac19696c4baa625f964d6
Ravaillack <matkowski.lucas@gmail.com>
2025-02-14 17:49:23 +0100

    merge depuis main

--- srcs/main.c
+++ srcs/main.c
@@ -18,51 +18,6 @@
 
 #include "minishell.h"
 
-<<<<<<< HEAD
-char	**ft_get_env(char **env_shell)
-{
-	char	**env;
-	int		i;
-
-	i = 0;
-	env = (char **)malloc(sizeof(char *));
-	if (!env)
-		return (NULL);
-	while (env_shell[i])
-	{
-		env[i] = ft_strdup(env_shell[i]);
-
-	}
-}
-
-t_minishell	*ft_init_var(char **env)
- {
-	t_minishell	*var;
-
-	var = (t_minishell *)malloc(sizeof(t_minishell));
-	if (!var)
-		return (NULL);
-	var->env = ft_get_env(env);
-	var->line = NULL;
-	return (var);
- }
-
-int	main(int argc, char **argv, char **env)
-{
-	t_minishell	*var;
-
-	(void)argc;
-	(void)argv;
-	var = ft_init_var(env);
-	while (1)
-	{
-		var->line = readline(PROMPT);
-		add_history(var->line);
-		//ft_error_line(var->line);
-	}
-	printf("%s\n", var->line);
-	free (var->line);
-=======
 void	handle_signal(int sig)
 {
 	if (sig == SIGINT)
@@ -104,5 +59,4 @@ int	main(void)
 		// read a new line from user input
 		str = readline(PROMPT);
 	}
->>>>>>> main
 }
