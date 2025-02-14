f3cd30ce2168140c606b3507e67ff3bdf783bfdd
sakamoto-42 <julien66140@hotmail.fr>
2025-02-14 15:42:54 +0100

    doc wip, first tests with signal and readline

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,22 +6,31 @@
 /*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 10:25:36 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/02/14 14:22:40 by juduchar         ###   ########.fr       */
+/*   Updated: 2025/02/14 15:09:09 by juduchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+// replace with ttyname ?
 # ifndef PROMPT
 #  define PROMPT "minishell$ "
 # endif
 
 # include <unistd.h>
-# include <stdio.h>
+
 # include <stdlib.h>
 # include <string.h>
+
+// for readline
+# include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+// for signal
+# include <signal.h>
+
 # include "libft.h"
 # include "get_next_line.h"
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,53 @@
 /*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 10:26:14 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/02/14 14:22:49 by juduchar         ###   ########.fr       */
+/*   Updated: 2025/02/14 15:37:47 by juduchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// first test with signal handling
+
 #include "minishell.h"
+
+void	handle_signal(int sig)
+{
+	if (sig == SIGINT)
+	{
+		printf("CTRL + C pressed\n");
+		// these functions always have to be together
+		// don't interpret the line before has a user input !
+		rl_on_new_line();
+		// give back the terminal to the user
+		rl_redisplay();
+	}
+}
 
 int	main(void)
 {
+	// user input
 	char	*str;
 
+	// handle signal CTRL + C
+	signal(SIGINT, handle_signal);
+	// get the user input
 	str = readline(PROMPT);
-	printf("%s\n", str);
-	free(str);
+	// while a user does not quit the shell
+	// TO DO : quit properly with a signal to quit
+	// do the signal handler and free all before quit
+	// clear history before quit ?
+	while (str)
+	{
+		// TO DO : handle EOF and input from file later
+		// check if we have to free str or not if the input is from a file ?
+		// TO DO : parser
+		if (*str)
+		{
+			// add the string to the history
+			add_history(str);
+			printf("You have entered %s\n", str);
+			free(str);
+		}
+		// read a new line from user input
+		str = readline(PROMPT);
+	}
 }
