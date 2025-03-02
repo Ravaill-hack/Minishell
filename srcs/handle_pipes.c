8327b62ef214534ab9006db2302afb7fe3c215df
Ravaillack <matkowski.lucas@gmail.com>
2025-03-02 20:55:40 +0100

    en cours

--- srcs/handle_pipes.c
+++ srcs/handle_pipes.c
@@ -6,7 +6,7 @@
 /*   By: Lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 09:07:11 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/03/02 19:35:55 by Lmatkows         ###   ########.fr       */
+/*   Updated: 2025/03/02 20:51:00 by Lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,53 @@ int	ft_need_to_grep_from_pipe(t_cmd **cmd_tab, int i_cmd, int nb_cmd)
 		|| cmd_tab[i_cmd - 1]->fd_out.is_def == 1)
 		return (0);
 	return (1);
+}
+
+int	ft_handle_last_cmd(t_var *var, t_shell shell, int i)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		return (FAILURE);
+	if (pid == 0)
+	{
+		if (dup2(var->cmd[i]->fd_out.fd, 1) == -1)
+			return (FAILURE);
+		ft_handle_cmd(var, shell, var->cmd[i]);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		if (dup2(var->cmd[i + 1]->fd_in.fd, 0) == -1)
+			return (FAILURE);
+	}
+}
+
+int	ft_handle_regular_cmd(t_var *var, t_shell shell, int i, int nb_cmd)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		return (FAILURE);
+	if (pid == 0)
+	{
+		if (var->cmd[i]->need_pipe_out == 1)
+			close(var->cmd[i + 1]->fd_in.fd);
+		if (dup2(var->cmd[i]->fd_out.fd, 1) == -1)
+			return (FAILURE);
+		ft_handle_cmd(var, shell, var->cmd[i]);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		if (var->cmd[i + 1]->need_pipe_in == 1)
+			close(var->cmd[i]->fd_out.fd);
+		if (dup2(var->cmd[i + 1]->fd_in.fd, 0) == -1)
+			return (FAILURE);
+	}
+	return (SUCCESS);
 }
 
 int	ft_handle_pipes(t_var *var, t_shell shell)
