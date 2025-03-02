e5c0941467be92353f7cbd7a59731602f41cf4ff
juduchar <juduchar@student.42perpignan.fr>
2025-03-02 12:53:28 +0100

    fix hanclde pipes

--- srcs/handle_cmd.c
+++ srcs/handle_cmd.c
@@ -6,57 +6,11 @@
 /*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 18:13:42 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/03/02 12:37:34 by julien           ###   ########.fr       */
+/*   Updated: 2025/03/02 12:52:44 by julien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-int	ft_set_pipes(t_var *var, t_cmd **cmd_list)
-{
-	int	i;
-	int	fd_pipe[2];
-
-	i = 1;
-	if (var->nb_cmd == 1)
-		return (SUCCESS);
-	if (var->nb_cmd == 2)
-		return (ft_set_2_pipes(var, cmd_list));
-	while (i < var->nb_cmd)
-	{
-		if (cmd_list[i]->fd_in.is_def != 1)
-		{
-
-		}
-	}
-	return (SUCCESS);
-}
-*/
-
-int	ft_handle_pipes(t_var *var, t_shell shell)
-{
-	int	i;
-	int	res;
-
-	i = 0;
-	res = 0;
-	while (i < var->nb_cmd)
-	{
-		if ((var->cmd[i])->fd_in.fd != 0)
-		{
-			if (dup2((var->cmd[i])->fd_in.fd, 0) == -1)
-				return (FAILURE);
-		}
-		if ((var->cmd[i])->fd_out.fd != 1)
-		{
-			if (dup2((var->cmd[i])->fd_out.fd, 1) == -1)
-				return (FAILURE);
-		}
-		res = ft_handle_cmd(var, shell, var->cmd[i]);
-		i++;
-	}
-	return (res);
-}
 
 int	ft_handle_cmd(t_var *var, t_shell shell, t_cmd *node)
 {
--- srcs/handle_pipes.c
+++ srcs/handle_pipes.c
@@ -6,13 +6,55 @@
 /*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 09:07:11 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/03/02 12:36:31 by julien           ###   ########.fr       */
+/*   Updated: 2025/03/02 12:53:01 by julien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_handle_pipes(void)
+/*
+int	ft_set_pipes(t_var *var, t_cmd **cmd_list)
 {
-	ft_printf("");
+	int	i;
+	int	fd_pipe[2];
+
+	i = 1;
+	if (var->nb_cmd == 1)
+		return (SUCCESS);
+	if (var->nb_cmd == 2)
+		return (ft_set_2_pipes(var, cmd_list));
+	while (i < var->nb_cmd)
+	{
+		if (cmd_list[i]->fd_in.is_def != 1)
+		{
+
+		}
+	}
+	return (SUCCESS);
+}
+*/
+
+int	ft_handle_pipes(t_var *var, t_shell shell)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (i < var->nb_cmd)
+	{
+		if ((var->cmd[i])->fd_in.fd != 0)
+		{
+			if (dup2((var->cmd[i])->fd_in.fd, 0) == -1)
+				return (FAILURE);
+		}
+		if ((var->cmd[i])->fd_out.fd != 1)
+		{
+			if (dup2((var->cmd[i])->fd_out.fd, 1) == -1)
+				return (FAILURE);
+		}
+		res = ft_handle_cmd(var, shell, var->cmd[i]);
+		i++;
+	}
+	return (res);
 }
