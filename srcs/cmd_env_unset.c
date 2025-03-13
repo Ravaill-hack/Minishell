c10313bb4f1a7bb20a3f7b5e0e311a0b1dbfdcfe
juduchar <juduchar@student.42perpignan.fr>
2025-03-13 21:20:12 +0100

    fix

--- srcs/cmd_env_unset.c
+++ srcs/cmd_env_unset.c
@@ -6,7 +6,7 @@
 /*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:44:08 by juduchar          #+#    #+#             */
-/*   Updated: 2025/03/13 20:39:08 by julien           ###   ########.fr       */
+/*   Updated: 2025/03/13 21:14:26 by julien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@
 
 int	ft_cmd_env(char **env, t_cmd *cmd_node)
 {
+	char	*path_value;
+
 	if (!cmd_node->arg[1])
 	{
 		if (ft_get_line_env(env, "PATH") == -1)
 		{
 			ft_putstr_fd("env: No such file or directory\n", 2);
 			return (127);
+		}
+		path_value = ft_extract_env_value_from_key(env, "PATH");
+		if (!path_value[0])
+		{
+			ft_print_strs(env);
+			return (SUCCESS);
 		}
 		ft_print_strs(env);
 		return (SUCCESS);
